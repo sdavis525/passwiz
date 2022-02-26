f306fd2b682a56cd21c50fd27f8d410132f9c922
Sophia Davis <sophiadavis@Sophias-MBP.attlocal.net>
2022-02-26 12:44:06 -0500

    getchoices is working

--- assets/js/passwiz.js
+++ assets/js/passwiz.js
@@ -1,22 +1,17 @@
-
 // Assignment code here
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-function generatePassword() {
- console.log("Hooray! It worked!")
 
  
  //lowercase, uppercase, numbers, special characters
- var specialCharters = ['!','@','#','$','%','^','&','*','+',];
+ var special = ['!','@','#','$','%','^','&','*','+',]
 
- var numericCharters = ['0','1','2','3','4','5','6','7','8','9',];
+ var numeric = ['0','1','2','3','4','5','6','7','8','9',]
  
- var lowerdCasedCharters = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m',];
+ var lower = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m',]
  
- var upperCasedCharters = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M',];
+ var upper = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M',]
  
 // an object of user choices that we will get from user
 var userChoices = {
@@ -25,92 +20,97 @@ var userChoices = {
     upperCase: false,
     numeric: false,
     special: false
-  }
+}
   
   // an function that gets user choices and saves them to our object
   function getChoices() {
     // user choices
-    var length = parseInt(prompt("how many characters"))
-    var lower = confirm("would you like lower")
-    var upper = confirm("would you like upper")
-    var numeric = confirm("would you like numeric")
-    var special = confirm("would you like special")
+    userChoices.length = parseInt(prompt("how many characters"))
+    userChoices.lowerCase = confirm("would you like lower")
+    userChoices.upperCase = confirm("would you like upper")
+    userChoices.numeric = confirm("would you like numeric")
+    userChoices.special = confirm("would you like special")
+  }
     
-    // make sure there is at least 8 characters
-    if (length >= 8 && length <= 128) {
-      userChoices.length = length
-    } else {
-      alert('chose an incorrect value, setting to 8 characters')
-    }
+   
+   
+   getChoices()
+   
+   // make sure there is at least 8 characters
+    
+  console.log(userChoices)
+   
+   //if (userChoices.length >= 8 && userChoices.length <= 128) {
+      //userChoices.length = length
+    //} else {
+    //  alert('chose an incorrect value, setting to 8 characters')
+    //}
   
-    // if user wants specific option, set our object to reflect that
-    if (lower) {
-      userChoices.lowerCase = true;
-    }
-  
-    if (upper) {
-      userChoices.upperCase = true;
-    }
-    
-    if (numeric) {
-      userChoices.numeric = true;
-    }
-    
-    if (special) {
-      userChoices.special = true;
-    }
   
     // generate a password
-    createPassword()
-  }
-  
-  // function to generate password
-  function createPassword() {
-    // our password to be
-    var password = ""
-  
-    // if user choice object contains chosen opti
-    if (userChoices.lowerCase) {
-      password += "abc";
+
+    
+    function generatePassword() {
+        var password = ""
+        var choicesArr = []
+        if (userChoices.special) {
+          choicesArr.push(special)
+        }
+        if (userChoices.lower) {
+          choicesArr.push(lower)
+        }
+        if (userChoices.numeric) {
+          choicesArr.push(numeric)
+        }
+        if (userChoices.upper) {
+            choicesArr.push(upper)
+        }
+        for (var i=0; i<userChoices.length; i++) {
+        
+            console.log(choicesArr)
+            password += getCharacter(choicesArr[Math.floor(Math.random() * choicesArr.length)])
+        }
+       
     }
-    if (userChoices.upperCase) {
-      password += "ABC";
-    }
-    if (userChoices.numeric) {
-      password += "123";
-    }
-    if (userChoices.special) {
-      password += "!@#"
-    }
-  
-    console.log(password)
-  }
-  
-  getChoices()
+
+      
+      function getCharacter(arr) {
+       
+       
+        var randIndex = Math.floor(Math.random() * arr.length);
+        var character = arr[randIndex]
+        return character
+       
+      }
+      
+        
 
 
-
-
-
-
-
-
-
- return "password will go here!";
-
-}
 
 // Write password to the #password input
 function writePassword() {
-            
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
 
-  passwordText.value = password;
 
-}
+  //generatePassword() 
+ 
+  //getChoices()
+     
+
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+//var generateBtn=document.querySelector("#generate")
+
+
+//generateBtn.addEventListener("click", writePassword);
+
+
+
 
 
